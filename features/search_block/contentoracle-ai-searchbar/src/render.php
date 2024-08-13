d80fb97dba6a378fb0e6701131ad00a19bf3e26f
jacob <jacobgraham@woosterbrush.com>
2024-08-13 15:36:40 -0400

    testing

--- features/search_block/contentoracle-ai-searchbar/src/render.php
+++ features/search_block/contentoracle-ai-searchbar/src/render.php
@@ -4,19 +4,36 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+$new_attributes = WP_Block_Supports::get_instance()->apply_block_supports();
+var_dump($new_attributes);
+echo "<br>";
+echo "<br>";
+
 //generate an instance id
 $instance_id = uniqid();
 
-//create styles for each element
-$button_styles = [];
+var_dump(get_block_wrapper_attributes());
+echo "<br>";
+echo "<br>";
+var_dump($attributes);
+echo "<br>";
+echo "<br>";
+//var_dump($content);
+//echo "<br>";
+//echo "<br>";
+//var_dump($block);
+
 
 $input_styles = [];
 
 $label_styles = [];
 
-$root_styles = [];
+$root_styles = [
+    'border' => ( isset($style_attrs['border']['width']) ? $style_attrs['border']['width'] : "0px" )
+    . " " . 
+    ( isset($style_attrs['border']['color']) ? $style_attrs['border']['color'] : "black" ),
+];
 
-var_dump( get_block_wrapper_attributes() );
 ?>
 
 
@@ -24,6 +41,6 @@ var_dump( get_block_wrapper_attributes() );
     <label class="contentoracle-ai_search_label" for="contentoracle_search_input_<?php echo esc_attr($instance_id) ?>">Search</label>
     <form action="#" method="POST" class="contentoracle-ai_search_container">
         <input class="contentoracle-ai_search_input" type="search" id="contentoracle_search_input_<?php echo esc_attr($instance_id) ?>" required>
-        <input type="submit" class="contentoracle-ai_search_button" value="Search">
+        <input type="submit" class="<?php echo esc_attr( $button_classes ) ?>" style="<?php echo esc_attr($button_styles) ?>" value="Search">
     </form>
 </div>
