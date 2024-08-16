2d2a836a6e29687bb272d3f9a63a25f5892fe050
jacob <jacobgraham@woosterbrush.com>
2024-08-16 15:54:46 -0400

    add redirect to ai search page when ai search key is filled

--- features/search_block/search_block.php
+++ features/search_block/search_block.php
@@ -16,6 +16,7 @@ class ContentOracleSearchBlock extends PluginFeature{
 
     public function add_actions(){
         add_action('init', array($this, 'register_search_blocks'));
+        add_action('pre_get_posts', array($this, 'handle_ai_search_request'));
     }
 
     //  \\  //  \\  //  \\  //  \\  //  \\  //  \\  //  \\  //  \\
@@ -27,6 +28,37 @@ class ContentOracleSearchBlock extends PluginFeature{
 
         //register search block
         register_block_type($this->get_base_dir() . '/features/search_block/contentoracle-ai-searchbar/build');
+    }
+
+    //handle a search request
+    public function handle_ai_search_request($query){
+        // Check if it's the main query and not in the admin area
+        if ($query->is_main_query() && !is_admin() && isset($_GET['contentoracle_ai_search'])) {
+            // Get the ai_search query parameter
+            $ai_search_query = sanitize_text_field($_GET['contentoracle_ai_search']);
+
+            //redirect to the ai search results page, if set, with the ai search query
+            $ai_results_page_id = get_option($this->get_prefix() . 'ai_results_page', null);
+            $ai_results_page = get_post($ai_results_page_id);
+
+            if ($ai_results_page && $ai_results_page->post_type == 'page' && $ai_results_page->post_status == 'publish') {
+
+                //ensure we only redirect if we are not already on the ai search results page
+                if ( !isset($query->query['page_id']) || $query->query['page_id'] != $ai_results_page_id ) {
+                    $ai_results_page_url = get_permalink($ai_results_page_id);
+                    $ai_search_url = add_query_arg('contentoracle_ai_search', $ai_search_query, $ai_results_page_url);
+                    wp_redirect($ai_search_url );
+                    exit;
+                }
+
+            }
+            //otherwise, redirect to orinary search results page
+            else {
+                $search_url = home_url('/?s=' . $ai_search_query);
+                wp_redirect($search_url);
+                exit;
+            }
+        }
     }
 
 }
