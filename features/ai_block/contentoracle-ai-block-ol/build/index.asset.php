e8ec3a894850d924ae168cd5c4fd608f5ac58116
jacob <jacobgraham@woosterbrush.com>
2024-08-23 14:44:27 -0400

    remove old block

--- features/ai_block/contentoracle-ai-block-ol/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '993d1e6c9e41b3292d61');
--- features/ai_block/contentoracle-ai-block-ol/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '75fb491afc227718f3b9');
