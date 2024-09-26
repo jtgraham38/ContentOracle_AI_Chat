0e841191c1fa3a70689d827723edf1cf5243f899
jacob <jacobgraham@woosterbrush.com>
2024-09-26 13:02:56 -0400

    readd dompurify and marked

--- features/chat_block/block/src/view.js
+++ features/chat_block/block/src/view.js
@@ -142,7 +142,7 @@ Alpine.data('contentoracle_ai_chat', () => ({
 						console.log(json);
 						this.conversation.push( {
 							role: 'assistant',
-							content: json.response,
+							content: DOMPurify.sanitize( marked( json.response ) ),
 							context_used: json.context_used,
 							context_supplied: json.context_supplied,
 							action: json.action
