55d2fcb3ec87041f839462160cee29d9a5022718
jacob <jacobgraham@woosterbrush.com>
2024-09-27 16:15:47 -0400

    debug html

--- features/wp_api/ContentOracleApiConnection.php
+++ features/wp_api/ContentOracleApiConnection.php
@@ -50,7 +50,7 @@ class ContentOracleApiConnection{
                 'client_ip' => $this->client_ip
                 
             )),
-            'timeout' => 2500,
+            'timeout' => 250,
         );
 
         
