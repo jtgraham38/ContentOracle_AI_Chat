d06f47f1a36e47711fa46562f6232070246566fb
jacob <jacobgraham@woosterbrush.com>
2025-03-12 08:36:28 -0400

    show options for post meta key values

--- features/settings/elements/ai/post_types_input.php
+++ features/settings/elements/ai/post_types_input.php
@@ -5,6 +5,8 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+global $wpdb;
+
 //get all post types
 $post_types = get_post_types(array(), 'objects');
 
@@ -41,7 +43,7 @@ $post_types_setting = get_option($this->get_prefix() . 'post_types');
         <div style="width: 40%; margin-left: 1rem;  display: inline-block;">
             <h4>Set Meta Keys to Prompt With</h4>
             <div style="max-height: 18rem; overflow-y: auto;">
-                <p>For each post type, enter the meta keys of the post meta you would like the ai to use to generate a response.  For example, you  might send the date of an event, or the publish date of a post.  Enter each key in the text input for a post type.  Separate mutliple keys with commas.</p>
+                <p>For each post type, enter the meta keys of the post meta you would like the ai to use to generate a response.  For example, you  might select the date of an event, or the publish date of a post.  Enter each key in the text input for a post type.  Separate multiple keys with commas.</p>
                 <?php
                     if (count($post_types_setting) > 0):
                         foreach ($post_types_setting as $i=>$post_type):
@@ -58,6 +60,32 @@ $post_types_setting = get_option($this->get_prefix() . 'post_types');
                                     value="<?php echo esc_attr( implode( "," ,get_option( $this->get_prefix() . $post_type . '_prompt_meta_keys') ) ) ; ?>" 
                                     style="width: 100%;" 
                                 />
+                                <details>
+                                    <summary>Options:</summary>
+                                    <ul>
+                                        <?php
+                                            //get all possible values for the post meta for this post type
+                                            $meta_keys = $wpdb->get_results("SELECT DISTINCT meta_key FROM $wpdb->postmeta WHERE post_id IN (SELECT ID FROM $wpdb->posts WHERE post_type = '$post_type')", ARRAY_A);
+                                            
+                                            //remove contentoracle ai chat specific keys
+                                            $exclude = array('coai_chat_embeddings', 'coai_chat_should_generate_embeddings');
+                                            $meta_keys = array_filter($meta_keys, function($meta_key) use ($exclude){
+                                                return !in_array($meta_key['meta_key'], $exclude);
+                                            });
+                                            
+                                            //show the meta keys
+                                            foreach ($meta_keys as $meta_key):
+                                            ?>
+                                                <li>
+                                                    <code>
+                                                    <?php echo esc_html($meta_key['meta_key']); ?>
+                                                    </code>
+                                                </li>
+                                            <?php
+                                            endforeach;
+                                        ?>
+                                    </ul>
+                                </details>
                             </div>
                         <?php
                         endforeach;
