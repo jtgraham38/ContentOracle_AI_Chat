de0b643424eddddaadf7a9a8363012e0472fab64
jacob <jacobgraham@woosterbrush.com>
2025-06-19 11:21:38 -0400

    update composer dependencies to jgwordpresskit v2.0.0 and refactor feature registration in contentoracle_ai_chat.php for improved clarity

--- contentoracle_ai_chat.php
+++ contentoracle_ai_chat.php
@@ -33,47 +33,47 @@ $plugin = new Plugin($prefix, plugin_dir_path( __FILE__ ), plugin_dir_url( __FIL
 //admin menu feature
 require_once plugin_dir_path(__FILE__) . 'features/admin_menu/feature.php';
 $feature = new ContentOracleMenu();
-$plugin->register_feature($feature);
+$plugin->register_feature('admin_menu', $feature);
 
 //settings feature
 require_once plugin_dir_path(__FILE__) . 'features/settings/feature.php';
 $feature = new ContentOracleSettings();
-$plugin->register_feature($feature); 
+$plugin->register_feature('settings', $feature); 
 
 //api feature
 require_once plugin_dir_path(__FILE__) . 'features/wp_api/feature.php';
 $feature = new ContentOracleApi();
-$plugin->register_feature($feature);
+$plugin->register_feature('wp_api', $feature);
 
 //register searchbar blocks feature
 require_once plugin_dir_path(__FILE__) . 'features/search_block/register_block.php';
 $feature = new ContentOracleSearchBlock();
-$plugin->register_feature($feature);
+$plugin->register_feature('search_block', $feature);
 
 //register main ai chat block
 require_once plugin_dir_path(__FILE__) . 'features/chat_block/register_block.php';
 $feature = new ContentOracleAiBlock();
-$plugin->register_feature($feature);
+$plugin->register_feature('chat_block', $feature);
 
 //register embeddings feature
 require_once plugin_dir_path(__FILE__) . 'features/embeddings/feature.php';
 $feature = new ContentOracleEmbeddings();
-$plugin->register_feature($feature);
+$plugin->register_feature('embeddings', $feature);
 
 //register the analytics feature
 require_once plugin_dir_path(__FILE__) . 'features/analytics/feature.php';
 $feature = new ContentOracleAnalytics();
-$plugin->register_feature($feature);
+$plugin->register_feature('analytics', $feature);
 
 //register the shortcodes feature
 require_once plugin_dir_path(__FILE__) . 'features/shortcodes/feature.php';
 $feature = new ContentOracleShortcodes();
-$plugin->register_feature($feature);
+$plugin->register_feature('shortcodes', $feature);
 
 //register the setup wizard feature
 require_once plugin_dir_path(__FILE__) . 'features/setup_wizard/feature.php';
 $feature = new ContentOracleSetupWizard();
-$plugin->register_feature($feature);
+$plugin->register_feature('setup_wizard', $feature);
 
 
 
