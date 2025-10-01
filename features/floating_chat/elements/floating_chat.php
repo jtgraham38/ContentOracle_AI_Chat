3ca52b496f5dafca4b153a397ac7da4c848f7595
jacob <jacobgraham@woosterbrush.com>
2025-10-01 15:34:09 -0400

    Refactor floating chat widget area ID to use a prefixed method for improved consistency and maintainability.

--- features/floating_chat/elements/floating_chat.php
+++ features/floating_chat/elements/floating_chat.php
@@ -5,7 +5,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Get the widget area ID
-$widget_area_id = 'coai_chat_floating_chat_widget_area';
+$widget_area_id = $this->prefixed('floating_chat_widget_area');
 
 // Check if the widget area has widgets
 if (is_active_sidebar($widget_area_id)) {
